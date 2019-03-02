4f9f2779fb5e0f6d711e3135113ecfbf8584daad
Alex Chauncy <contactcharleschauncy@gmail.com>
2019-03-02 16:16:02 -0600

    Adds documentation to the parser.

--- Juno/parser.py
+++ Juno/parser.py
@@ -6,12 +6,28 @@ today = datetime.date.today()
 tomorrow = today + datetime.timedelta(days=1)
 overmorrow = today + datetime.timedelta(days=2)
 
+##
+# Returns one day's weather
+#
+# @param jsonOneDay Json string representing exactly one day of weather.
+# @return           Description, high, and low for the day in json format.
 def getDailyWeather(jsonOneDay):
-    return {"description": jsonOneDay["description"], "high": jsonOneDay["highTemperature"], "low": jsonOneDay["lowTemperature"]}
+    return {"description": jsonOneDay["description"], "high": jsonOneDay["highTemperature"],
+            "low": jsonOneDay["lowTemperature"]}
 
+##
+# Returns one day's date
+#
+# @param jsonOneDay Json string representing exactly one day of weather.
+# @return           Utc date for the input day.
 def getDate(jsonOneDay):
     return jsonOneDay["utcTime"][:10]
 
+##
+# Returns a three day forecast.
+#
+# @param rawWeatherDump Raw json string returned from the Amazon weather api.
+# @return               Three days of description, high, and low for the day in json format.
 def getForecast(rawWeatherDump):
     jsonWeatherData = json.loads(str({}))
     dayList = rawWeatherDump["daily"]
@@ -26,6 +42,12 @@ def getForecast(rawWeatherDump):
             jsonWeatherData["overmorrow"] = getDailyWeather(day)
     return jsonWeatherData
 
+##
+# Returns a the Juno approved weather forecast.
+#
+# @param rawWeatherDump Raw json string returned from the Amazon weather api.
+# @return               Forecasted weather in an easily parsable format.
 def parseData(rawWeatherDump):
     jsonToday = rawWeatherDump["today"]
-    return {"state": jsonToday["state"], "city": jsonToday["city"], "day": str(today), "weather": getForecast(rawWeatherDump)}
+    return {"state": jsonToday["state"], "city": jsonToday["city"], "day": str(today),
+            "weather": getForecast(rawWeatherDump)}
