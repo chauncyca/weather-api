8efd227d0d33d6fc4ac1344e30cfe34aec0605bc
Alex Chauncy <contactcharleschauncy@gmail.com>
2019-03-03 02:23:02 -0600

    Adds basic UDP server functionality.
    *Extends socketserver which provides a built in message queue.
    *Adds some logic for handling messages.

--- Juno/server.py
+++ Juno/server.py
@@ -1,12 +1,45 @@
 #!/usr/bin/python
 
-import socket
-import asyncio
-import websockets
+import datetime
+import socketserver
 
-UDP_IP = "127.0.0.1"
+from . import handler
+
+HOST = "127.0.0.1"
 UDP_PORT = 55400
 
-#SOCK = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#SOCK.bind((UDP_IP, UDP_PORT))
+STAY_ALIVE = True
+LOG_FILE = "visitors.txt"
 
+
+##
+# Class: JunoListener extends socketserver.BaseRequestHandler
+#        Provides a synchronous queue and handling of incoming events.
+class JunoListener(socketserver.BaseRequestHandler):
+    def handle(self):
+        data = self.request[0]
+        socket = self.request[1]
+
+        response = {}
+
+        if data["city"] and data["state"]:
+            response = handler.getWeather(data)
+        else:
+            try:
+                handler.updateCache(data)
+            # Blanket catch is used here because a messages other than
+            # what we expect are a "don't care" state.
+            except:
+                pass
+
+        socket.sendto(response, self.client_address)
+
+    def log(self, city, state):
+        with open(LOG_FILE, "a") as f:
+            outstring = datetime.date.today() + ", " + city + ", " + state
+            f.write(outstring)
+
+
+def run():
+    server = socketserver.UDPServer((HOST, UDP_PORT), JunoListener)
+    server.serve_forever()
