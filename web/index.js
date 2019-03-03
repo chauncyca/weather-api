8d84b5ab389ced913df0dbb0280f412ab90f3e4f
Alex Chauncy <makeanewname@gmail.com>
2019-03-03 02:55:01 -0600

    Begins adding the front end.

---
+++ web/index.js
@@ -0,0 +1,20 @@
+// Create WebSocket connection.
+//const socket = new WebSocket('ws://' + window.location.host + ':8080');
+
+var junoRequest;
+$.ajax({
+  url: "http://ip-api.com/json",
+  type: 'GET',
+  success: function(json)
+  {
+    junoRequest = '{'
+                  +'"city":"' + json.city +'", '
+                  +'"state":"' + json.regionName +'", '
+                  + '}';
+    console.log(junoRequest);
+  },
+  error: function(err)
+  {
+    console.log("Addressing failed, error= " + err);
+  }
+});
