9311310c965bd8425aa3db4138bfc16eec09c4d0
Alex Chauncy <contactcharleschauncy@gmail.com>
2019-03-05 00:39:50 -0600

    Moves scripts to the scripts folder.
    Adds install script which grabs necessary python modules.

---
+++ scripts/run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+#NOTE: Execution directory is the project top level.
+#      This script is basically not needed, it is just here encase someone doesn't want to execute the .py
+python main.py
