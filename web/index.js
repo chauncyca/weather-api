f2abfb434f897682556644497cee21c5cd5d059c
Alex Chauncy <contactcharleschauncy@gmail.com>
2019-03-05 00:34:56 -0600

    Front end now displays correct data.

--- web/index.js
+++ web/index.js
@@ -1,76 +1,96 @@
 // Create WebSocket connection.
 //const socket = new WebSocket('ws://' + window.location.host + ':8080');
-const socket = new WebSocket('ws://127.0.0.1:8080');
-console.log("Listening")
 
-socket.onclose = function(e){ console.log("Socket connection closed"); };
-socket.onmessage = function(e){
-                                    console.log("Socket message recieved", e.data);
-                                    parseResponse(e.data);
-                              };
-socket.onerror = function(e) {console.log("Socket error", e.data);};
-
-var junoRequest;
-
-getLocation();
-
-socket.onopen = ()=> socket.send(junoRequest)
-
-function parseResponse(response) {
-    var jsonResponse = JSON.parse(response)
-    console.log("Json response: ", jsonResponse);
-    if(jsonResponse.action == "sendReq") {
-        getWeatherFromApi();
-    }
-    else if (jsonResponse.action == "updatedCache"){
-        console.log("Juno req: ", junoRequest);
-        socket.send(junoRequest);
-    }
-    else {
-        console.log("Bad response: ", response);
-    }
-}
-
-function getWeatherFromApi() {
-    const Http = new XMLHttpRequest();
-    const url =  "https://j9l4zglte4.execute-api.us-east-1.amazonaws.com/api/ctl/weather/"
-    Http.open("GET", url);
-    Http.send();
+function retrieveWeather(obj) {
     
-    Http.onreadystatechange=(e)=> {
-        socket.send(Http.responseText)
+    const socket = new WebSocket('ws://127.0.0.1:8080');
+
+    socket.onclose = function(e){ console.log("Socket connection closed"); };
+    socket.onmessage = function(e){
+                                        console.log("Socket message recieved", e.data);
+                                        parseResponse(e.data);
+                                  };
+    socket.onerror = function(e) {console.log("Socket error", e.data);};
+
+    var output = document.getElementById('output');
+    var junoRequest;
+    var dataToDisplay;
+
+    getLocation();
+
+    socket.onopen = ()=> socket.send(junoRequest)
+
+    function parseResponse(response) {
+        var jsonResponse = JSON.parse(response)
+        console.log("Json response: ", jsonResponse);
+        if(jsonResponse.action == "sendReq") {
+            getWeatherFromApi();
+        }
+        else if (jsonResponse.action == "updatedCache"){
+            console.log("Juno req: ", junoRequest);
+            socket.send(junoRequest);
+        }
+        else if (jsonResponse.action == "failure") {
+            console.log("Error cannot be handled, terminating.")
+            socket.onclose = function () {}
+            socket.close()
+        }
+        else if (jsonResponse){
+            dataToDisplay = jsonResponse;
+            output.innerHTML = response;
+            console.log("Retrieved data. Closing socket.")
+            socket.onclose = function () {}
+            socket.close()
+        }
+        else {
+            console.log("Bad response: ", response);
+        }
     }
-//    $.ajax({
-//        url: "https://j9l4zglte4.execute-api.us-east-1.amazonaws.com/api/ctl/weather/",
-//        type: 'GET',
-//        success: function(json)
-//        {
-//            console.log(json.)
-//            socket.send(json)
-//        },
-//        error: function(err)
-//        {
-//            console.log("Failed to aquire weather data, error= " + err);
-//        }
-//    });
+
+    function getWeatherFromApi() {
+        const Http = new XMLHttpRequest();
+        const url =  "https://j9l4zglte4.execute-api.us-east-1.amazonaws.com/api/ctl/weather/"
+        Http.open("GET", url);
+        Http.send();
+
+        Http.onreadystatechange=(e)=> {
+            socket.send(Http.responseText)
+        }
+    }
+
+    function getLocation(){
+        $.ajax({
+            url: "http://ip-api.com/json",
+            type: 'GET',
+            async: false,
+            success: function(json)
+            {
+                junoRequest = '{"action":"retrieve",'
+                          +'"city":"' + json.city +'", '
+                          +'"state":"' + json.regionName + '"'
+                          + '}';
+            },
+            error: function(err)
+            {
+                console.log("Addressing failed, error= " + err);
+            }
+        });
+    }
 }
 
-function getLocation(){
-    $.ajax({
-    url: "http://ip-api.com/json",
-    type: 'GET',
-    async: false,
-    success: function(json)
-    {
-        junoRequest = '{"action":"retrieve",'
-                  +'"city":"' + json.city +'", '
-                  +'"state":"' + json.regionName + '"'
-                  + '}';
-    },
-    error: function(err)
-    {
-        console.log("Addressing failed, error= " + err);
-    }
-});
+function getJSON(callback) {
+    var xhr = new XMLHttpRequest(),
+        method = 'GET';
+    xhr.addEventListener('load', function (e) {
+        callback(this.response);
+    });
+    xhr.open(method, "index.html");
+    xhr.responseType = 'json';
+    xhr.send();
 }
+
+window.addEventListener('load', function () {
+    getJSON(retrieveWeather)
+});
+
 
