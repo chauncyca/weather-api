314ca86f1547356be328a814716d8c6c55986eee
Alex Chauncy <contactcharleschauncy@gmail.com>
2019-03-05 23:51:57 -0600

    Adds clear comments to the server file.

--- Juno/httpserver.py
+++ Juno/httpserver.py
@@ -9,23 +9,39 @@ from . import config
 from . import handler
 
 
+##
+# Class: JunoHttpServer
+#        extends http.server.BaseHTTPRequestHandler
+#
+#        Provides access to a simple HTTP server that handles GET requests and page views.
 class JunoHttpServer(http.server.BaseHTTPRequestHandler):
+
+    ##
+    # Sets header data and the HTTP response code.
     def _setHeaders(self):
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
 
+    ##
+    # Prints JSON weather data to the webpage when accessed.
     def do_HEAD(self):
         self._returnData()
 
+    ##
+    # Responds to GET requests with JSON weather data.
     def do_GET(self):
         self._returnData()
 
+    ##
+    # Method to retrieve and write the data to the page.
     def _returnData(self):
         self._setHeaders()
         self.output = handler.getWeather()
         self.wfile.write(json.dumps(self.output).encode())
 
+##
+# Sets logging variables and starts the server.
 def run():
     logging.basicConfig(filename=config.ERROR_LOG)
     serverClass = http.server.HTTPServer
