e35ab5da52190d4690c1dc28137e9ac92768036b
Alex Chauncy <contactcharleschauncy@gmail.com>
2019-03-05 23:42:25 -0600

    Add the WEATHER_API_URL to the config

--- Juno/config.py
+++ Juno/config.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
-HOST = "127.0.0.1"
-SOCKET_PORT = 8080
+HOST = "localhost"
+PORT = 8080
 LOG_FILE = "logs/visitors.txt"
 CACHE_LOCATION = "logs/cache.json"
 ERROR_LOG = "logs/error.log"
-FAILURE_MESSAGE = {"action":"sendReq"}
+WEATHER_API_URL = "https://j9l4zglte4.execute-api.us-east-1.amazonaws.com/api/ctl/weather/"
